df7ef935879ca3ab3e347f9196afaf028f1b464e
henrisja <henrisja@umich.edu>
2021-12-28 14:26:11 -0500

    get a basic three scene on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+import { SceneManager } from './features/SceneManager';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SceneManager />;
 }
 
 export default App;
