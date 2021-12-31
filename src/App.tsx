16ced87e5a2dba17dd856ae599bbaff6a5389941
henrisja <henrisja@umich.edu>
2021-12-30 23:17:07 -0500

    forgot to stage

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,19 @@
 import * as React from 'react';
 import { css, jsx } from '@emotion/react';
 import { Canvas } from '@react-three/fiber';
-import Box from './features/scenecomponents/foreground/Box';
 import WelcomeScreen from './features/ui/welcomescreen';
 import SongControls from './features/ui/songcontrols';
+import SceneManager from './features/scene/scenemanager';
 import useStore from './features/store/store';
 
-const SceneManager: React.FC = () => {
+const MainMenu: React.FC = () => {
   const audioElement = useStore((state) => state.audioElement);
-
-  // TODO - see if this can be done w/o a useEffect hook
-  React.useEffect(() => {
-    if (!audioElement) {
-      return;
-    }
-
-    audioElement.play();
-  }, [audioElement]);
 
   return audioElement ? (
     <div css={sceneContainerStyling}>
       <SongControls />
       <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <SceneManager />
       </Canvas>
     </div>
   ) : (
@@ -41,7 +29,7 @@ const sceneContainerStyling = css({
 });
 
 const App: React.FC = () => {
-  return <SceneManager />;
+  return <MainMenu />;
 };
 
 export default App;
